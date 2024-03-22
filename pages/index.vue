815acd25f093cce4dd823041fbd09e014d5599fc
Daniel Ahn <et3rnal.d@gmail.com>
2024-03-22 16:21:32 -0700

    basic module setup

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useMouse } from '@vueuse/core';
+
+const { x, y } = useMouse();
+const color = useColorMode();
+
+function toggleColorMode() {
+  color.value = color.value === 'dark'? 'light' : 'dark';
+}
+</script>
+
+<template>
+  <div class="text-red">Hello World {{ x }}, {{ y }} <br />
+  <client-only>
+    <button @click="toggleColorMode">{{ color.value }}</button>
+  </client-only>
+</div>
+</template>
